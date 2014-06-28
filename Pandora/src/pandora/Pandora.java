ef2f1c5c3398a1424c73a1b2fa232b1d3cca9149
Dominik Mocher <dominik.mocher@student.tugraz.at>
2014-06-28 18:09:12 +0200

    bla bla

--- Pandora/src/pandora/Pandora.java
+++ Pandora/src/pandora/Pandora.java
@@ -6,7 +6,17 @@
 
 package pandora;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javax.swing.JApplet;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -17,6 +27,10 @@ import javax.swing.UIManager;
  * @author dominikmocher
  */
 public class Pandora extends JApplet {
+    
+    private static final int JFXPANEL_WIDTH_INT = 300;
+    private static final int JFXPANEL_HEIGHT_INT = 250;
+    private static JFXPanel fxContainer;
 
     /**
      * @param args the command line arguments
@@ -53,6 +67,8 @@ public class Pandora extends JApplet {
                 //TEMPORARY CODE
                 //main.add(mainframe);
                 main.setVisible(true);
+                //implement validate
+                //main.pack(); // delete because of validate
                 
                 /*
                 JFrame frame = new JFrame("JavaFX 2 in Swing");
@@ -74,6 +90,41 @@ public class Pandora extends JApplet {
                 */
             }
         });
+    }
+    
+    @Override
+    public void init() {
+        fxContainer = new JFXPanel();
+        fxContainer.setPreferredSize(new Dimension(JFXPANEL_WIDTH_INT, JFXPANEL_HEIGHT_INT));
+        add(fxContainer, BorderLayout.CENTER);
+        // create JavaFX scene
+        Platform.runLater(new Runnable() {
+            
+            @Override
+            public void run() {
+                createScene();
+            }
+        });
+    }
+    
+    private void createScene() {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+        
+
+                
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        fxContainer.setScene(new Scene(root));
     }
     
 }
